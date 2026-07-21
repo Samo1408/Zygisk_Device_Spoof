9745a95d3b469ec818e07260a168de59fe6e9375
Samo1408 <ab.0ss0mm@gmail.com>
2026-07-21 19:42:32 +0300

    fix: Module build.gradle.kts with AGP app plugin + cmake

--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -1,38 +1,79 @@
 plugins {
-    id("com.android.library")
+    alias(libs.plugins.agp.app)
 }
 
+val moduleId: String by rootProject.extra
+val moduleName: String by rootProject.extra
+val verCode: Int by rootProject.extra
+val verName: String by rootProject.extra
+val commitHash: String by rootProject.extra
+val abiList: List<String> by rootProject.extra
+
 android {
-    namespace = "com.samo.zygisk_device_spoof"
-    compileSdk = 34
-    ndkVersion = "27.0.12077973"
     defaultConfig {
-        minSdk = 26
+        ndk {
+            abiFilters.addAll(abiList)
+        }
         externalNativeBuild {
             cmake {
-                arguments("-DMODULE_NAME=zygisk_device_spoof")
-                abiFilters("arm64-v8a", "armeabi-v7a", "x86_64", "x86")
-                cppFlags("-std=c++20 -fno-exceptions -fno-rtti")
+                cppFlags("-std=c++20", "-Wno-deprecated-declarations")
+                arguments(
+                    "-DANDROID_STL=c++_static",
+                    "-DMODULE_NAME=$moduleId"
+                )
             }
         }
     }
-    externalNativeBuild { cmake { path("src/main/cpp/CMakeLists.txt"); version = "3.22.1" } }
     buildTypes {
-        debug { externalNativeBuild { cmake { cppFlags.add("-DDEBUG") } } }
-        release { externalNativeBuild { cmake { cppFlags.add("-O3 -DNDEBUG") } } }
+        debug {
+            externalNativeBuild {
+                cmake { cppFlags("-DDEBUG") }
+            }
+        }
+        release { }
+    }
+    externalNativeBuild {
+        cmake { path("src/main/cpp/CMakeLists.txt") }
     }
 }
-val zipDebug by tasks.registering(Zip::class) {
-    dependsOn("assembleDebug")
-    archiveFileName.set("zygisk_device_spoof_debug.zip")
-    destinationDirectory.set(file("$projectDir/release"))
-    from("$projectDir/template")
-    from("${layout.buildDirectory}/intermediates/stripped_native_libs/debug/out/lib") { into("zygisk") }
-}
-val zipRelease by tasks.registering(Zip::class) {
-    dependsOn("assembleRelease")
-    archiveFileName.set("zygisk_device_spoof_release.zip")
-    destinationDirectory.set(file("$projectDir/release"))
-    from("$projectDir/template")
-    from("${layout.buildDirectory}/intermediates/stripped_native_libs/release/out/lib") { into("zygisk") }
+
+androidComponents.onVariants { variant ->
+    afterEvaluate {
+        val variantLowered = variant.name.lowercase()
+        val variantCapped = variant.name.replaceFirstChar { it.uppercase() }
+        val buildTypeLowered = variant.buildType?.lowercase()
+
+        val moduleDir = layout.buildDirectory.file("outputs/module/$variantLowered")
+        val zipFileName = "$moduleName-$verName-$verCode-$commitHash-$buildTypeLowered.zip".replace(' ', '-')
+
+        val prepareModuleFilesTask = tasks.register<Sync>("prepareModuleFiles$variantCapped") {
+            group = "module"
+            dependsOn("assemble$variantCapped")
+            into(moduleDir)
+            from(rootProject.layout.projectDirectory.file("README.md"))
+            from(layout.projectDirectory.file("template")) {
+                exclude("module.prop")
+            }
+            from(layout.projectDirectory.file("template")) {
+                include("module.prop")
+                expand(
+                    "moduleId" to moduleId,
+                    "moduleName" to moduleName,
+                    "versionName" to "$verName ($verCode-$commitHash-$variantLowered)",
+                    "versionCode" to verCode
+                )
+            }
+            from(layout.buildDirectory.file("intermediates/stripped_native_libs/$variantLowered/strip${variantCapped}DebugSymbols/out/lib")) {
+                into("lib")
+            }
+        }
+
+        val zipTask = tasks.register<Zip>("zip$variantCapped") {
+            group = "module"
+            dependsOn(prepareModuleFilesTask)
+            archiveFileName.set(zipFileName)
+            destinationDirectory.set(layout.projectDirectory.file("release").asFile)
+            from(moduleDir)
+        }
+    }
 }
