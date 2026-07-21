0b76bb55a5f4974c916484420f3ee8b1b8a151c6
Samo1408 <ab.0ss0mm@gmail.com>
2026-07-21 18:38:06 +0300

    v2.0: Updated post-fs-data.sh for proc spoofing

--- module/template/post-fs-data.sh
+++ module/template/post-fs-data.sh
@@ -1,1 +1,10 @@
-MODDIR=${0%/*}
+#!/system/bin/sh
+MODDIR=/bin
+MODID=zygisk_device_spoof
+PROC_DIR="/proc"
+if [ -f "/cpuinfo" ]; then
+    mount --bind "/cpuinfo" /proc/cpuinfo 2>/dev/null
+fi
+if [ -f "/meminfo" ]; then
+    mount --bind "/meminfo" /proc/meminfo 2>/dev/null
+fi
