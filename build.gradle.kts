0e8b538f02ca3b937d2aaab37a019790eeeda1d7
Samo1408 <ab.0ss0mm@gmail.com>
2026-07-21 19:41:47 +0300

    fix: Restore original AGP 8.11.0 root build with v2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,66 @@
+import com.android.build.gradle.AppExtension
+import java.io.ByteArrayOutputStream
+
 plugins {
-    id("com.android.library") version "8.2.0" apply false
+    alias(libs.plugins.agp.app) apply false
+}
+
+fun String.execute(currentWorkingDir: File = file("./")): String {
+    val byteOut = ByteArrayOutputStream()
+    project.exec {
+        workingDir = currentWorkingDir
+        commandLine = split("\\s".toRegex())
+        standardOutput = byteOut
+    }
+    return String(byteOut.toByteArray()).trim()
+}
+
+val gitCommitCount = try { "git rev-list HEAD --count".execute().toInt() } catch (_: Exception) { 200 }
+val gitCommitHash = try { "git rev-parse --verify --short HEAD".execute() } catch (_: Exception) { "unknown" }
+
+val moduleId by extra("zygisk_device_spoof")
+val moduleName by extra("Zygisk Device Spoof")
+val verName by extra("v2.0.0")
+val verCode by extra(gitCommitCount)
+val commitHash by extra(gitCommitHash)
+val abiList by extra(listOf("arm64-v8a", "armeabi-v7a"))
+
+val androidMinSdkVersion by extra(26)
+val androidTargetSdkVersion by extra(34)
+val androidCompileSdkVersion by extra(34)
+val androidBuildToolsVersion by extra("34.0.0")
+val androidCompileNdkVersion by extra("27.0.12077973")
+val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
+val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
+
+tasks.register("Delete", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+fun Project.configureBaseExtension() {
+    extensions.findByType(AppExtension::class)?.run {
+        namespace = "io.github.qjj0204.zygisk.device.spoof"
+        compileSdkVersion(androidCompileSdkVersion)
+        ndkVersion = androidCompileNdkVersion
+        buildToolsVersion = androidBuildToolsVersion
+        defaultConfig {
+            minSdk = androidMinSdkVersion
+        }
+        compileOptions {
+            sourceCompatibility = androidSourceCompatibility
+            targetCompatibility = androidTargetCompatibility
+        }
+    }
+}
+
+subprojects {
+    plugins.withId("com.android.application") {
+        configureBaseExtension()
+    }
+    plugins.withType(JavaPlugin::class.java) {
+        extensions.configure(JavaPluginExtension::class.java) {
+            sourceCompatibility = androidSourceCompatibility
+            targetCompatibility = androidTargetCompatibility
+        }
+    }
 }
